22a5713ed3f47a95cd549aee7ea1d93f464fbca6
CrisFiss <crs.hdz.s@gmail.com>
2020-06-19 19:16:51 -0500

    first commit

---
+++ app/src/main/java/edmt/dev/androideatitv2shipper/MainActivity.java
@@ -0,0 +1,14 @@
+package edmt.dev.androideatitv2shipper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
